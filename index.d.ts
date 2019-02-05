f17a8e8efeccdd773f04d1cdbc68b990bfcae9f5
Jannik Keye <jannik.keye@gmail.com>
2019-02-05 03:10:54 +0100

    feat: add typings (#90)
    * feat: add typings
    
    * fix: adjust ci script
    
    * fix: adjust typings to use fastify.Plugin
    
    * test: case for sendFile
    
    * fix: add node types to fix ci typescript  check
    
    * fix: add definitions contributors to definition file
    
    * fix: remove obsolete script part

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+// Definitions by: Jannik <https://github.com/jannikkeye>
+//                 Leo <https://github.com/leomelzer>
+
+import fastify = require("fastify");
+
+import { Server, IncomingMessage, ServerResponse } from "http";
+
+declare module "fastify" {
+    interface FastifyReply<HttpResponse> {
+        sendFile(filename: string): FastifyReply<HttpResponse>;
+    }
+}
+
+declare const fastifyStatic: fastify.Plugin<Server, IncomingMessage, ServerResponse, {
+    root: string;
+    prefix?: string;
+    serve?: boolean;
+    decorateReply?: boolean;
+    schemaHide?: boolean;
+    setHeaders?: (...args: any[]) => void;
+
+    // Passed on to `send`
+    acceptRanges?: boolean;
+    cacheControl?: boolean;
+    dotfiles?: boolean;
+    etag?: boolean;
+    extensions?: string[];
+    immutable?: boolean;
+    index?: string[];
+    lastModified?: boolean;
+    maxAge?: string | number;
+}>;
+
+export = fastifyStatic;
---
+++ test/types/index.ts
@@ -0,0 +1,28 @@
+import * as Fastify from 'fastify'
+import * as fastifyStatic from '../..'
+
+const app = Fastify()
+
+app.register(fastifyStatic, {
+  acceptRanges: true,
+  cacheControl: true,
+  decorateReply: true,
+  dotfiles: true,
+  etag: true,
+  extensions: ['.js'],
+  immutable: true,
+  index: ['1'],
+  lastModified: true,
+  maxAge: '',
+  prefix: '',
+  root: '',
+  schemaHide: true,
+  serve: true,
+  setHeaders: (res: any, pathName: any) => {
+    res.setHeader('test', pathName)
+  }
+})
+
+app.get('/file', (request, reply) => {
+  reply.sendFile('some-file-name')
+})
