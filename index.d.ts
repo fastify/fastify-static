b0bbc9e9e2f6a693de1994bb3375f8886a43ad1f
Max <rtwatmi@gmail.com>
2019-11-22 00:03:34 +0300

    included https package

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,9 @@ import * as fastify from 'fastify';
 import { Plugin } from "fastify";
 import { Server, IncomingMessage, ServerResponse } from "http";
 import { Http2SecureServer, Http2Server, Http2ServerRequest, Http2ServerResponse } from "http2";
+import * as https from "https";
 
-type HttpServer = Server | Http2Server | Http2SecureServer;
+type HttpServer = Server | Http2Server | Http2SecureServer | https.Server;
 type HttpRequest = IncomingMessage | Http2ServerRequest;
 type HttpResponse = ServerResponse | Http2ServerResponse;
 
