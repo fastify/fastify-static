3482500a3f1832638c60ababa0baac797c9865c3
Christian Todd <christian.todd7@gmail.com>
2021-04-14 20:24:42 -0400

    fix formatting of typedef file.

--- index.d.ts
+++ index.d.ts
@@ -2,84 +2,80 @@
 //                 Leo <https://github.com/leomelzer>
 /// <reference types="node" />
 
-import { FastifyPluginCallback, FastifyReply } from "fastify"
+import { FastifyPluginCallback, FastifyReply } from 'fastify';
 
 declare module "fastify" {
   interface FastifyReply {
-    sendFile(filename: string, rootPath?: string): FastifyReply
-    download(filepath: string, options?: SendOptions): FastifyReply
-    download(filepath: string, filename?: string): FastifyReply
-    download(
-      filepath: string,
-      filename?: string,
-      options?: SendOptions
-    ): FastifyReply
+    sendFile(filename: string, rootPath?: string): FastifyReply;
+    download(filepath: string, options?: SendOptions): FastifyReply;
+    download(filepath: string, filename?: string): FastifyReply;
+    download(filepath: string, filename?: string, options?: SendOptions): FastifyReply;
   }
 }
 
 interface ListDir {
-  href: string
-  name: string
+  href: string;
+  name: string;
 }
 
 interface ListFile {
-  href: string
-  name: string
+  href: string;
+  name: string;
 }
 
 interface ListRender {
-  (dirs: ListDir[], files: ListFile[]): string
+  (dirs: ListDir[], files: ListFile[]): string;
 }
 
 interface ListOptions {
-  format: "json" | "html"
-  names: string[]
-  render: ListRender
+  format: 'json' | 'html';
+  names: string[];
+  render: ListRender;
 }
 
 // Passed on to `send`
 interface SendOptions {
-  acceptRanges?: boolean
-  cacheControl?: boolean
-  dotfiles?: "allow" | "deny" | "ignore"
-  etag?: boolean
-  extensions?: string[]
-  immutable?: boolean
-  index?: string[] | false
-  lastModified?: boolean
-  maxAge?: string | number
+  acceptRanges?: boolean;
+  cacheControl?: boolean;
+  dotfiles?: 'allow' | 'deny' | 'ignore';
+  etag?: boolean;
+  extensions?: string[];
+  immutable?: boolean;
+  index?: string[] | false;
+  lastModified?: boolean;
+  maxAge?: string | number;
 }
 
 export interface FastifyStaticOptions extends SendOptions {
-  root: string | string[]
-  prefix?: string
-  prefixAvoidTrailingSlash?: boolean
-  serve?: boolean
-  decorateReply?: boolean
-  schemaHide?: boolean
-  setHeaders?: (...args: any[]) => void
-  redirect?: boolean
-  wildcard?: boolean
-  list?: boolean | ListOptions
+  root: string | string[];
+  prefix?: string;
+  prefixAvoidTrailingSlash?: boolean;
+  serve?: boolean;
+  decorateReply?: boolean;
+  schemaHide?: boolean;
+  setHeaders?: (...args: any[]) => void;
+  redirect?: boolean;
+  wildcard?: boolean;
+  list?: boolean | ListOptions;
+  allowedPath?: (pathName: string, root?: string) => boolean;
   /**
    * @description
    * Opt-in to looking for pre-compressed files
    */
-  preCompressed?: boolean
-  allowedPath?: (pathName: string, root?: string) => boolean
+  preCompressed?: boolean;
 
   // Passed on to `send`
-  acceptRanges?: boolean
-  cacheControl?: boolean
-  dotfiles?: "allow" | "deny" | "ignore"
-  etag?: boolean
-  extensions?: string[]
-  immutable?: boolean
-  index?: string[] | false
-  lastModified?: boolean
-  maxAge?: string | number
+  acceptRanges?: boolean;
+  cacheControl?: boolean;
+  dotfiles?: 'allow' | 'deny' | 'ignore';
+  etag?: boolean;
+  extensions?: string[];
+  immutable?: boolean;
+  index?: string[] | false;
+  lastModified?: boolean;
+  maxAge?: string | number;
 }
 
 declare const fastifyStatic: FastifyPluginCallback<FastifyStaticOptions>
 
-export default fastifyStatic
+export default fastifyStatic;
