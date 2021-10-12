16eef579bbe5a7352e4968e610f9b52dbc5a77d4
KaKa <kaka@kakawebsitedemo.com>
2021-10-12 12:12:45 +0800

    test: 100% coverage

--- index.js
+++ index.js
@@ -155,6 +155,8 @@ async function fastifyStatic (fastify, opts) {
         try {
           reply.redirect(301, getRedirectUrl(request.raw.url))
         } catch (error) {
+          // the try-catch here is actually unreachable, but we keep it for safety and prevent DoS attack
+          /* istanbul ignore next */
           reply.send(error)
         }
       } else {
@@ -458,8 +460,12 @@ function getRedirectUrl (url) {
     const parsed = new URL(url, 'http://localhost.com/')
     return parsed.pathname + (parsed.pathname[parsed.pathname.length - 1] !== '/' ? '/' : '') + (parsed.search || '')
   } catch (error) {
+    // the try-catch here is actually unreachable, but we keep it for safety and prevent DoS attack
+    /* istanbul ignore next */
     const err = new Error(`Invalid redirect URL: ${url}`)
+    /* istanbul ignore next */
     err.statusCode = 400
+    /* istanbul ignore next */
     throw err
   }
 }
