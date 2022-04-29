6b4332b3389a5768af37dac9c9a88e626ef1461e
Frazer Smith <frazer.dev@outlook.com>
2022-04-29 15:04:15 +0100

    build(deps-dev): use new @fastify org dependencies

--- example/server-compress.js
+++ example/server-compress.js
@@ -5,7 +5,7 @@ const fastify = require('fastify')({ logger: { level: 'trace' } })
 
 fastify
   // Compress everything.
-  .register(require('fastify-compress'), { threshold: 0 })
+  .register(require('@fastify/compress'), { threshold: 0 })
   .register(require('../'), {
     // An absolute path containing static files to serve.
     root: path.join(__dirname, '/public')
